stefano-garzarella/svsm

scripts/launch_guest.sh: support proxy serial

scripts/launch_guest.sh
@@ -12,7 +12,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 : "${IGVM:=$SCRIPT_DIR/../bin/coconut-qemu.igvm}"
 
 C_BIT_POS=`$SCRIPT_DIR/../utils/cbit`
-DEBUG_SERIAL=""
+DEBUG_SERIAL="-serial null"
+PROXY_SERIAL="-serial null"
 QEMU_EXIT_DEVICE=""
 
 while [[ $# -gt 0 ]]; do
@@ -34,6 +35,11 @@ while [[ $# -gt 0 ]]; do
       ;;
     -d|--debugserial)
       DEBUG_SERIAL="-serial pty"
+      shift
+      ;;
+    -p|--proxy)
+      PROXY_SERIAL="-serial unix:$2"
+      shift
       shift
       ;;
     --unit-tests)
@@ -110,6 +116,7 @@ $SUDO_CMD \
     -monitor none \
     -serial stdio \
     $DEBUG_SERIAL \
+    $PROXY_SERIAL \
     $QEMU_EXIT_DEVICE
 
 stty intr ^C

osteffenrh/coconut-svsm

scripts/launch_guest.sh
@@ -14,7 +14,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 C_BIT_POS=`$SCRIPT_DIR/../utils/cbit`
 COM1_SERIAL="-serial stdio" # console
 COM2_SERIAL="-serial null"  # debug
-COM3_SERIAL="-serial null"  # used by hyper-v
+COM3_SERIAL="-serial null"  # used by hyper-v and proxy on Qemu
 COM4_SERIAL="-serial null"  # used by in-SVSM tests
 QEMU_EXIT_DEVICE=""
 QEMU_TEST_IO_DEVICE=""
@@ -47,6 +47,11 @@ while [[ $# -gt 0 ]]; do
       ;;
     -d|--debugserial)
       COM2_SERIAL="-serial pty"
+      shift
+      ;;
+    -p|--proxy)
+      COM3_SERIAL="-serial unix:$2"
+      shift
       shift
       ;;
     --unit-tests)