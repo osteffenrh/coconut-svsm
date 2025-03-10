30a14a444b37a57788b8fc3a45c12a35c6aa7dc6
Oliver Steffen <osteffen@redhat.com>
2025-03-10 22:18:20 +0100

    spinlock: add locked_do()
    Add a method to spinlock that executes a supplied function while holding
    the lock. It is automatically released afterwards.
    
    Signed-off-by: Oliver Steffen <osteffen@redhat.com>

--- kernel/src/locking/spinlock.rs
+++ kernel/src/locking/spinlock.rs
@@ -169,6 +169,24 @@ impl<T, I: IrqLocking> RawSpinLock<T, I> {
         }
     }
 
+    /// Execute function F while holding the lock.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use svsm::locking::SpinLock;
+    ///
+    /// let spin_lock = SpinLock::new(42);
+    ///
+    /// // Do some actions while holding the lock.
+    /// // Lock is automatically taken and released.
+    /// let value = spin_lock.locked_do(|s| s+= 1; s);
+    /// ```
+    pub fn locked_do<R, F: FnMut(&mut T) -> R>(&self, mut f: F) -> R {
+        let mut l = self.lock();
+        f(&mut (*l))
+    }
+
     /// This method tries to acquire the lock without blocking. If the
     /// lock is not available, it returns `None`. If the lock is
     /// successfully acquired, it returns a [`LockGuard`] that automatically
