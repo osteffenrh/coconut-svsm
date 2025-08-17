stefano-garzarella/svsm

blockfs: add metadata on the first sector

kernel/src/fs/blockfs.rs
@@ -6,9 +6,12 @@ use crate::locking::{RWLock, SpinLock};
 use crate::mm::PerCPUPageMappingGuard;
 use crate::virtio::SvsmHal;
 
+use core::cmp::min;
 use core::ops::DerefMut;
 use core::ptr::NonNull;
 
+use postcard;
+use serde::{Deserialize, Serialize};
 use virtio_drivers::{
     device::blk::{VirtIOBlk, SECTOR_SIZE},
     transport::mmio::{MmioTransport, VirtIOHeader},
@@ -127,15 +130,81 @@ macro_rules! try0 {
     };
 }
 
-#[derive(Debug, Default)]
-struct RawBlockFile {
-    sector: usize,
+#[derive(Serialize, Deserialize, Debug)]
+struct RawMetadata {
+    magic: u32,
     size: usize,
 }
 
+impl RawMetadata {
+    const MAGIC: u32 = 0xDEADBEEF;
+
+    fn check(&self) -> bool {
+        self.magic == Self::MAGIC
+    }
+}
+
+impl Default for RawMetadata {
+    fn default() -> Self {
+        RawMetadata {
+            magic: Self::MAGIC,
+            size: 0,
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct RawBlockFile {
+    sector: usize,
+    capacity: usize,
+    metadata: RawMetadata,
+}
+
 impl RawBlockFile {
-    pub fn new(sector: usize, size: usize) -> Self {
-        RawBlockFile { sector, size }
+    pub fn new(sector: usize, capacity: usize) -> Self {
+        let mut file = RawBlockFile {
+            sector,
+            capacity,
+            metadata: RawMetadata::default(),
+        };
+
+        file.read_metadata().unwrap();
+        file
+    }
+
+    fn read_metadata(&mut self) -> Result<(), FsError> {
+        let device_guard = BLOCK_DEVICE.lock_read();
+        let device = device_guard.as_ref().ok_or(FsError::Inval)?;
+
+        let mut buf = [0u8; SECTOR_SIZE];
+        device.read_blocks(self.sector, &mut buf)?;
+
+        self.metadata = match postcard::from_bytes::<RawMetadata>(&buf) {
+            Ok(metadata) => {
+                if metadata.check() {
+                    metadata
+                } else {
+                    RawMetadata::default()
+                }
+            }
+            Err(_) => RawMetadata::default(),
+        };
+
+        Ok(())
+    }
+
+    fn write_metadata(&self) -> Result<(), FsError> {
+        let device_guard = BLOCK_DEVICE.lock_read();
+        let device = device_guard.as_ref().ok_or(FsError::Inval)?;
+
+        let mut buf = [0u8; SECTOR_SIZE];
+        let _ = postcard::to_slice(&self.metadata, &mut buf).unwrap();
+        device.write_blocks(self.sector, &buf)
+    }
+
+    fn update_size(&mut self, size: usize) -> Result<(), FsError> {
+        self.metadata.size = size;
+        self.write_metadata()
     }
 
     fn read_blocks(
@@ -144,7 +213,7 @@ impl RawBlockFile {
         block_id: usize,
         buf: &mut [u8],
     ) -> Result<(), FsError> {
-        device.read_blocks(self.sector + block_id, buf)
+        device.read_blocks(self.sector + 1 + block_id, buf)
     }
 
     fn write_blocks(
@@ -153,10 +222,10 @@ impl RawBlockFile {
         block_id: usize,
         buf: &[u8],
     ) -> Result<(), FsError> {
-        device.write_blocks(self.sector + block_id, buf)
+        device.write_blocks(self.sector + 1 + block_id, buf)
     }
 
-    fn read(&self, buf: &mut [u8], offset: usize) -> Result<usize, SvsmError> {
+    fn read_device(&self, buf: &mut [u8], offset: usize) -> Result<usize, SvsmError> {
         let device_guard = BLOCK_DEVICE.lock_read();
         let device = device_guard
             .as_ref()
@@ -176,8 +245,8 @@ impl RawBlockFile {
                 // Read to target buf directly
                 try0!(len, self.read_blocks(device, range.block, buf));
             } else {
-                let mut block_buf = [0u8; 1 << 10];
-                assert!(device.block_size_log2() <= 10);
+                let mut block_buf = [0u8; SECTOR_SIZE];
+                assert!(device.block_size_log2() as u32 <= SECTOR_SIZE.ilog2());
                 // Read to local buf first
                 try0!(len, self.read_blocks(device, range.block, &mut block_buf));
                 // Copy to target buf then
@@ -187,7 +256,7 @@ impl RawBlockFile {
         Ok(buf.len())
     }
 
-    fn write(&self, buf: &[u8], offset: usize) -> Result<usize, SvsmError> {
+    fn write_device(&mut self, buf: &[u8], offset: usize) -> Result<usize, SvsmError> {
         let device_guard = BLOCK_DEVICE.lock_read();
         let device = device_guard
             .as_ref()
@@ -208,8 +277,8 @@ impl RawBlockFile {
                 try0!(len, self.write_blocks(device, range.block, buf));
             } else {
                 #[allow(clippy::uninit_assumed_init)]
-                let mut block_buf = [0u8; 1 << 10];
-                assert!(device.block_size_log2() <= 10);
+                let mut block_buf = [0u8; SECTOR_SIZE];
+                assert!(device.block_size_log2() as u32 <= SECTOR_SIZE.ilog2());
                 // Read to local buf first
                 try0!(len, self.read_blocks(device, range.block, &mut block_buf));
                 // Write to local buf
@@ -218,19 +287,55 @@ impl RawBlockFile {
                 try0!(len, self.write_blocks(device, range.block, &block_buf));
             }
         }
+
         Ok(buf.len())
     }
 
-    fn truncate(&self, size: usize) -> Result<usize, SvsmError> {
-        if size > self.size {
+    fn read(&self, buf: &mut [u8], offset: usize) -> Result<usize, SvsmError> {
+        if offset > self.capacity {
             return Err(SvsmError::from(FsError::Inval));
         }
 
-        Ok(self.size)
+        if offset > self.metadata.size {
+            return Ok(0);
+        }
+
+        let len = min(self.metadata.size - offset, buf.len());
+
+        self.read_device(&mut buf[..len], offset)
+    }
+
+    fn write(&mut self, buf: &[u8], offset: usize) -> Result<usize, SvsmError> {
+        if offset + buf.len() > self.capacity {
+            return Err(SvsmError::from(FsError::Inval));
+        }
+
+        let written = self.write_device(buf, offset)?;
+
+        if offset + written > self.metadata.size {
+            self.update_size(offset + written)?;
+        }
+
+        Ok(written)
+    }
+
+    fn truncate(&mut self, size: usize) -> Result<usize, SvsmError> {
+        if size > self.capacity {
+            return Err(SvsmError::from(FsError::Inval));
+        }
+
+        // TODO: support it writing zeros
+        if size > self.metadata.size {
+            return Err(SvsmError::FileSystem(FsError::inval()));
+        }
+
+        self.update_size(size)?;
+
+        Ok(size)
     }
 
     fn size(&self) -> usize {
-        self.size
+        self.metadata.size
     }
 }
 

osteffenrh/coconut-svsm

kernel/src/fs/blockfs.rs
@@ -7,7 +7,11 @@ use crate::fw_cfg::FwCfg;
 use crate::locking::RWLock;
 use crate::platform::SVSM_PLATFORM;
 
+use core::cmp::min;
 use core::ops::DerefMut;
+use postcard;
+use serde::{Deserialize, Serialize};
+use virtio_drivers::device::blk::SECTOR_SIZE;
 
 unsafe impl Send for VirtIOBlkDriver {}
 unsafe impl Sync for VirtIOBlkDriver {}
@@ -95,15 +99,81 @@ macro_rules! try0 {
     };
 }
 
-#[derive(Debug, Default)]
-struct RawBlockFile {
-    sector: usize,
+#[derive(Serialize, Deserialize, Debug)]
+struct RawMetadata {
+    magic: u32,
     size: usize,
 }
 
+impl RawMetadata {
+    const MAGIC: u32 = 0xDEADBEEF;
+
+    fn check(&self) -> bool {
+        self.magic == Self::MAGIC
+    }
+}
+
+impl Default for RawMetadata {
+    fn default() -> Self {
+        RawMetadata {
+            magic: Self::MAGIC,
+            size: 0,
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct RawBlockFile {
+    sector: usize,
+    capacity: usize,
+    metadata: RawMetadata,
+}
+
 impl RawBlockFile {
-    pub fn new(sector: usize, size: usize) -> Self {
-        RawBlockFile { sector, size }
+    pub fn new(sector: usize, capacity: usize) -> Self {
+        let mut file = RawBlockFile {
+            sector,
+            capacity,
+            metadata: RawMetadata::default(),
+        };
+
+        file.read_metadata().unwrap();
+        file
+    }
+
+    fn read_metadata(&mut self) -> Result<(), SvsmError> {
+        let device_guard = BLOCK_DEVICE.lock_read();
+        let device = device_guard.as_ref().ok_or(FsError::Inval)?;
+
+        let mut buf = [0u8; SECTOR_SIZE];
+        device.read_blocks(self.sector, &mut buf)?;
+
+        self.metadata = match postcard::from_bytes::<RawMetadata>(&buf) {
+            Ok(metadata) => {
+                if metadata.check() {
+                    metadata
+                } else {
+                    RawMetadata::default()
+                }
+            }
+            Err(_) => RawMetadata::default(),
+        };
+
+        Ok(())
+    }
+
+    fn write_metadata(&self) -> Result<(), SvsmError> {
+        let device_guard = BLOCK_DEVICE.lock_read();
+        let device = device_guard.as_ref().ok_or(FsError::Inval)?;
+
+        let mut buf = [0u8; SECTOR_SIZE];
+        let _ = postcard::to_slice(&self.metadata, &mut buf).unwrap();
+        device.write_blocks(self.sector, &buf)
+    }
+
+    fn update_size(&mut self, size: usize) -> Result<(), SvsmError> {
+        self.metadata.size = size;
+        self.write_metadata()
     }
 
     fn read_blocks(
@@ -112,7 +182,7 @@ impl RawBlockFile {
         block_id: usize,
         buf: &mut [u8],
     ) -> Result<(), SvsmError> {
-        device.read_blocks(self.sector + block_id, buf)
+        device.read_blocks(self.sector + 1 + block_id, buf)
     }
 
     fn write_blocks(
@@ -121,10 +191,10 @@ impl RawBlockFile {
         block_id: usize,
         buf: &[u8],
     ) -> Result<(), SvsmError> {
-        device.write_blocks(self.sector + block_id, buf)
+        device.write_blocks(self.sector + 1 + block_id, buf)
     }
 
-    fn read(&self, buf: &mut [u8], offset: usize) -> Result<usize, SvsmError> {
+    fn read_device(&self, buf: &mut [u8], offset: usize) -> Result<usize, SvsmError> {
         let device_guard = BLOCK_DEVICE.lock_read();
         let device = device_guard
             .as_ref()
@@ -144,8 +214,8 @@ impl RawBlockFile {
                 // Read to target buf directly
                 try0!(len, self.read_blocks(device, range.block, buf));
             } else {
-                let mut block_buf = [0u8; 1 << 10];
-                assert!(device.block_size_log2() <= 10);
+                let mut block_buf = [0u8; SECTOR_SIZE];
+                assert!(device.block_size_log2() as u32 <= SECTOR_SIZE.ilog2());
                 // Read to local buf first
                 try0!(len, self.read_blocks(device, range.block, &mut block_buf));
                 // Copy to target buf then
@@ -155,7 +225,7 @@ impl RawBlockFile {
         Ok(buf.len())
     }
 
-    fn write(&self, buf: &[u8], offset: usize) -> Result<usize, SvsmError> {
+    fn write_device(&self, buf: &[u8], offset: usize) -> Result<usize, SvsmError> {
         let device_guard = BLOCK_DEVICE.lock_read();
         let device = device_guard
             .as_ref()
@@ -176,8 +246,8 @@ impl RawBlockFile {
                 try0!(len, self.write_blocks(device, range.block, buf));
             } else {
                 #[allow(clippy::uninit_assumed_init)]
-                let mut block_buf = [0u8; 1 << 10];
-                assert!(device.block_size_log2() <= 10);
+                let mut block_buf = [0u8; SECTOR_SIZE];
+                assert!(device.block_size_log2() as u32 <= SECTOR_SIZE.ilog2());
                 // Read to local buf first
                 try0!(len, self.read_blocks(device, range.block, &mut block_buf));
                 // Write to local buf
@@ -186,19 +256,55 @@ impl RawBlockFile {
                 try0!(len, self.write_blocks(device, range.block, &block_buf));
             }
         }
+
         Ok(buf.len())
     }
 
-    fn truncate(&self, size: usize) -> Result<usize, SvsmError> {
-        if size > self.size {
+    fn read(&self, buf: &mut [u8], offset: usize) -> Result<usize, SvsmError> {
+        if offset > self.capacity {
             return Err(SvsmError::from(FsError::Inval));
         }
 
-        Ok(self.size)
+        if offset > self.metadata.size {
+            return Ok(0);
+        }
+
+        let len = min(self.metadata.size - offset, buf.len());
+
+        self.read_device(&mut buf[..len], offset)
+    }
+
+    fn write(&mut self, buf: &[u8], offset: usize) -> Result<usize, SvsmError> {
+        if offset + buf.len() > self.capacity {
+            return Err(SvsmError::from(FsError::Inval));
+        }
+
+        let written = self.write_device(buf, offset)?;
+
+        if offset + written > self.metadata.size {
+            self.update_size(offset + written)?;
+        }
+
+        Ok(written)
+    }
+
+    fn truncate(&mut self, size: usize) -> Result<usize, SvsmError> {
+        if size > self.capacity {
+            return Err(SvsmError::from(FsError::Inval));
+        }
+
+        // TODO: support it writing zeros
+        if size > self.metadata.size {
+            return Err(SvsmError::FileSystem(FsError::inval()));
+        }
+
+        self.update_size(size)?;
+
+        Ok(size)
     }
 
     fn size(&self) -> usize {
-        self.size
+        self.metadata.size
     }
 }
 