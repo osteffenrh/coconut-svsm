coconut-svsm/svsm

locking: implement From<T> for RawSpinLock<T> + RawRWLock<T>
From<T> for RawSpinLock<T> or RawRWLock<T> is trivial to implement and
provides a convenient way following standard Rust idioms for
constructing these. Implement From<T> for RawSpinLock<T> and
RawRWLock<T>.

Signed-off-by: Nicolai Stange <nstange@suse.de>

kernel/src/locking/rwlock.rs
@@ -7,6 +7,7 @@
 use super::common::*;
 use crate::types::TPR_LOCK;
 use core::cell::UnsafeCell;
+use core::convert::From;
 use core::marker::PhantomData;
 use core::ops::{Deref, DerefMut};
 use core::sync::atomic::{AtomicU64, Ordering};
@@ -302,6 +303,12 @@ impl<T: Send + Sync, I: IrqLocking> RawRWLock<T, I> {
             data: unsafe { &*self.data.get() },
             _irq_state: irq_state,
         }
+    }
+}
+
+impl<T: Send, I: IrqLocking> From<T> for RawRWLock<T, I> {
+    fn from(value: T) -> Self {
+        RawRWLock::new(value)
     }
 }
 

osteffenrh/coconut-svsm

kernel/src/locking/rwlock.rs
@@ -7,6 +7,7 @@
 use super::common::*;
 use crate::types::TPR_LOCK;
 use core::cell::UnsafeCell;
+use core::convert::From;
 use core::marker::PhantomData;
 use core::ops::{Deref, DerefMut};
 use core::sync::atomic::{AtomicU64, Ordering};
@@ -302,6 +303,12 @@ impl<T: Send + Sync, I: IrqLocking> RawRWLock<T, I> {
             data: unsafe { &*self.data.get() },
             _irq_state: irq_state,
         }
+    }
+}
+
+impl<T: Send, I: IrqLocking> From<T> for RawRWLock<T, I> {
+    fn from(value: T) -> Self {
+        Self::new(value)
     }
 }
 